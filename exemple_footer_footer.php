92d132b5437b30669c67b4304ac6c3a62fec24e5
Arthur Galy <arthur.galy@lecole-ldlc.com>
2024-06-17 16:34:52 +0200

    j'ai changé le header et le footer et j'ai créé un exemple pour que vous sachiez l'utiliser

---
+++ exemple_footer_footer.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+   
+    <title>exemple_footer</title>
+
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+        <?php require_once(__DIR__ . '/header.php'); ?>
+
+
+
+
+
+
+
+
+
+
+        
+    <?php require_once(__DIR__ . '/footer.php'); ?>
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-light text-center text-lg-start mt-auto">
+    <div class="text-center p-3">
+        <a class="text-dark" href=""></a>
+    </div>
+</footer>
