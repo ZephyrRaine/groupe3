43a3716bf137477319d81c594658af2fedc73f78
Lmorder <thomas.roussel@lecole-ldlc.com>
2024-06-17 15:22:11 +0200

    Début du fichier de tempalte d'annonce

---
+++ templateAnnonce.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Description de votre site ou page">
+    <meta name="keywords" content="mots-clés, séparés, par, des, virgules">
+    <meta name="author" content="Votre Nom">
+    <link rel="stylesheet" href="templateAnnonce.css">
+    <title>Template d'une Annonce</title>
+    </head>
+<body>
+    <header>
+        <h1>Annonce</h1>
+    </header>
+    <main>
+        <?php
+            // Votre code PHP ici
+            echo "Bonjour, aujourd'hui nous sommes le " . date("d/m/Y") . ".";
+        ?>
+    </main>
+</body>
+</html>
