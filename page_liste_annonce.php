3132f75ca7692a36bcc68ea45ce028e235dd8633
Lmorder <thomas.roussel@lecole-ldlc.com>
2024-06-18 10:33:41 +0200

    Modifs Annonce (affiche totues les annonces, donc a modifier et intégrer des filtres etc) et page_liste_annonce (juste commencée sans contenu encore)

--- page_liste_annonce.php
+++ page_liste_annonce.php
@@ -1,3 +1,12 @@
+<?php
+// Démarrer la session au début du fichier
+session_start();
+
+// Inclure les fichiers nécessaires
+require_once(__DIR__ . '/BDD.php'); // Assurez-vous que ce fichier contient la connexion à la base de données
+require_once(__DIR__ . '/header.php');
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,12 +15,12 @@
     <meta name="description" content="Description de votre site ou page">
     <meta name="keywords" content="mots-clés, séparés, par, des, virgules">
     <meta name="author" content="Votre Nom">
-    <title>
-        Liste des Annonces
-    </title>
-
-    <?php
-    require_once(__DIR__ . '/BDD.php');
-    ?>
-
+    <title>Annonce</title>
 </head>
+<body>
+    <main>
+
+    
+
+    </main>
+</body>
