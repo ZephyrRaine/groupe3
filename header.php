a319d3abebe5b5e9097485994f0aad69f224babb
Arthur Galy <arthur.galy@lecole-ldlc.com>
2024-06-19 15:28:16 +0200

    header changé j'ai ajouté l'onglet connection.

--- header.php
+++ header.php
@@ -22,6 +22,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="profile.php">profile</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">connection</a>
+                </li>
             </ul>
        
        
